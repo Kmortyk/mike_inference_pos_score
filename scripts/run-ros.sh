5a4dcd00ab7ba6303776290e6d5142a235c7e03d
kmortyk <casha.dryaba@mail.ru>
2022-06-04 20:29:56 +0300

    fix

--- scripts/run-ros.sh
+++ scripts/run-ros.sh
@@ -7,7 +7,7 @@ set -Eeo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 project_dir="$(realpath "$script_dir"/..)"
 
-export PYTHONPATH="$project_dir:$project_dir/tfod2/models_c2793c1/research:$project_dir/tfod2/models_c2793c1/research/slim"
+export PYTHONPATH="$PYTHONPATH:$project_dir:$project_dir/tfod2/models_c2793c1/research:$project_dir/tfod2/models_c2793c1/research/slim"
 
 # start the inference
 python3 "$script_dir"/../src/ros/inference_module_atm.py
